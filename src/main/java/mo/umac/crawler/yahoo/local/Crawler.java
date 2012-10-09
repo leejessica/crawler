48922afe1444dfa52da7d1ab373b295fa39fbb1b
yanhui19 <kate.yanhui@gmail.com>
2012-10-09 18:30:35 +0800

    first commit

---
+++ src/main/java/mo/umac/crawler/yahoo/local/Crawler.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package mo.umac.crawler.yahoo.local;
+
+/**
+ * Conduct a search for a product or business name 
+ * within a specified zipcode using the Yahoo! Local search Service.
+ * @author yanhui
+ *
+ */
+public class Crawler {
+	//
+	private void localSearch(String business, String zipcode){
+		
+	}
+	
+	private int zipcode(){
+		return 0;
+	}
+	
+	private String business(){
+		return null;
+	}
+
+}
---
+++ src/main/java/mo/umac/crawler/yahoo/local/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @see http://developer.yahoo.com/search/local/V3/localSearch.html
+ */
+/**
+ * @author yanhui
+ *
+ */
+package mo.umac.crawler.yahoo.local;
