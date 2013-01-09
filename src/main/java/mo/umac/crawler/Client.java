bba9978cdf05adb0f3f22bee7835f21872d12eb9
kate <kate@kate-55.(none)>
2013-01-09 19:49:18 +0800

    Testing

--- src/main/java/mo/umac/crawler/Client.java
+++ src/main/java/mo/umac/crawler/Client.java
@@ -1,5 +1,7 @@
 package mo.umac.crawler;
 
+import org.apache.log4j.xml.DOMConfigurator;
+
 public class Client {
 
 	public static String LOG_PROPERTY_PATH = "./src/main/resources/log4j.xml";
@@ -8,6 +10,7 @@ public class Client {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		DOMConfigurator.configure(Client.LOG_PROPERTY_PATH);
 		CrawlerStrategy crawlerStrategy = new QuadTreeCrawler();
 		CrawlerContext crawlerContext = new CrawlerContext(crawlerStrategy);
 		crawlerContext.callCrawling();
