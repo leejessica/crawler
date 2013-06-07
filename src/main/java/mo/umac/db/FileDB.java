73fb4b0fd1b2d0fa2fab79aa0cc2466b629b381d
kateyim <kate.yan.code@gmail.com>
2013-06-07 19:45:08 +0800

    bring home

--- src/main/java/mo/umac/db/FileDB.java
+++ src/main/java/mo/umac/db/FileDB.java
@@ -19,140 +19,146 @@ import mo.umac.parser.ResultSet;
  */
 public class FileDB extends DataSet {
 
-	@Override
-	public void record(int queryID, int level, int parentID, YahooLocalQuery qc, ResultSet resultSet) {
-		writeQueryFile(Integer.toString(queryID), qc.getQueryOutput(), qc.queryInfo(), resultSet);
-		if (resultSet.getTotalResultsReturned() > 0) {
-			writeResultsFile(Integer.toString(queryID), resultSet);
-		}
+    @Override
+    public void record(int queryID, int level, int parentID,
+	    YahooLocalQuery qc, ResultSet resultSet) {
+	writeQueryFile(Integer.toString(queryID), qc.getQueryOutput(),
+		qc.queryInfo(), resultSet);
+	if (resultSet.getTotalResultsReturned() > 0) {
+	    writeResultsFile(Integer.toString(queryID), resultSet);
 	}
+    }
 
-	/**
-	 * @param partFileName
-	 * @param dbOutput
-	 * @param queryInfo
-	 * @param resultSet
-	 */
-	private void writeQueryFile(String partFileName, BufferedWriter dbOutput, String queryInfo, ResultSet resultSet) {
-		try {
-			dbOutput.write(partFileName);
-			dbOutput.write(";");
-			dbOutput.write(queryInfo);
-			dbOutput.write(";");
-			dbOutput.write(Integer.toString(resultSet.getTotalResultsAvailable()));
-			dbOutput.write(";");
-			dbOutput.write(Integer.toString(resultSet.getTotalResultsReturned()));
-			dbOutput.write(";");
-			dbOutput.write(Integer.toString(resultSet.getFirstResultPosition()));
-			dbOutput.newLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    /**
+     * @param partFileName
+     * @param dbOutput
+     * @param queryInfo
+     * @param resultSet
+     */
+    private void writeQueryFile(String partFileName, BufferedWriter dbOutput,
+	    String queryInfo, ResultSet resultSet) {
+	try {
+	    dbOutput.write(partFileName);
+	    dbOutput.write(";");
+	    dbOutput.write(queryInfo);
+	    dbOutput.write(";");
+	    dbOutput.write(Integer.toString(resultSet
+		    .getTotalResultsAvailable()));
+	    dbOutput.write(";");
+	    dbOutput.write(Integer.toString(resultSet.getTotalResultsReturned()));
+	    dbOutput.write(";");
+	    dbOutput.write(Integer.toString(resultSet.getFirstResultPosition()));
+	    dbOutput.newLine();
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
+    }
 
-	/**
-	 * @param partFileName
-	 * @param qc
-	 * @param resultSet
-	 *            basic information of the query.
-	 * @deprecated
-	 */
-	private void writeQueryFile(String partFileName, YahooLocalQuery qc, ResultSet resultSet) {
-		BufferedWriter dbOutput = qc.getQueryOutput();
-		String query = qc.getQuery();
-		int zip = qc.getZip();
-		int results = qc.getResults();
-		int start = qc.getStart();
-		double latitude = qc.getCircle().getCenter().y;
-		double longitude = qc.getCircle().getCenter().x;
-		double radius = qc.getCircle().getRadius();
-		try {
-			// file name:
-			dbOutput.write(partFileName);
-			dbOutput.write(";");
-			// other information:
-			dbOutput.write(query);
-			dbOutput.write(";");
-			dbOutput.write(Integer.toString(zip));
-			dbOutput.write(";");
-			dbOutput.write(Integer.toString(results));
-			dbOutput.write(";");
-			dbOutput.write(Integer.toString(start));
-			dbOutput.write(";");
-			dbOutput.write(Double.toString(latitude));
-			dbOutput.write(";");
-			dbOutput.write(Double.toString(longitude));
-			dbOutput.write(";");
-			dbOutput.write(Double.toString(radius));
-			dbOutput.write(";");
-			dbOutput.write(Integer.toString(resultSet.getTotalResultsAvailable()));
-			dbOutput.write(";");
-			dbOutput.write(Integer.toString(resultSet.getTotalResultsReturned()));
-			dbOutput.write(";");
-			dbOutput.write(Integer.toString(resultSet.getFirstResultPosition()));
-			dbOutput.newLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    /**
+     * @param partFileName
+     * @param qc
+     * @param resultSet
+     *            basic information of the query.
+     * @deprecated
+     */
+    private void writeQueryFile(String partFileName, YahooLocalQuery qc,
+	    ResultSet resultSet) {
+	BufferedWriter dbOutput = qc.getQueryOutput();
+	String query = qc.getQuery();
+	int zip = qc.getZip();
+	int results = qc.getResults();
+	int start = qc.getStart();
+	double latitude = qc.getCircle().getCenter().y;
+	double longitude = qc.getCircle().getCenter().x;
+	double radius = qc.getCircle().getRadius();
+	try {
+	    // file name:
+	    dbOutput.write(partFileName);
+	    dbOutput.write(";");
+	    // other information:
+	    dbOutput.write(query);
+	    dbOutput.write(";");
+	    dbOutput.write(Integer.toString(zip));
+	    dbOutput.write(";");
+	    dbOutput.write(Integer.toString(results));
+	    dbOutput.write(";");
+	    dbOutput.write(Integer.toString(start));
+	    dbOutput.write(";");
+	    dbOutput.write(Double.toString(latitude));
+	    dbOutput.write(";");
+	    dbOutput.write(Double.toString(longitude));
+	    dbOutput.write(";");
+	    dbOutput.write(Double.toString(radius));
+	    dbOutput.write(";");
+	    dbOutput.write(Integer.toString(resultSet
+		    .getTotalResultsAvailable()));
+	    dbOutput.write(";");
+	    dbOutput.write(Integer.toString(resultSet.getTotalResultsReturned()));
+	    dbOutput.write(";");
+	    dbOutput.write(Integer.toString(resultSet.getFirstResultPosition()));
+	    dbOutput.newLine();
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
+    }
 
-	private void writeResultsFile(String partFileName, ResultSet resultSet) {
-		BufferedWriter resultsOutput = resultSet.getResultsOutput();
-		List<Result> results = resultSet.getResults();
-		for (int i = 0; i < results.size(); i++) {
-			Result result = results.get(i);
-			int id = result.getId();
-			String title = result.getTitle();
-			String address = result.getAddress();
-			String city = result.getCity();
-			String state = result.getState();
-			String phone = result.getPhone();
-			Double longitude = result.getLongitude();
-			Double latitude = result.getLatitude();
+    private void writeResultsFile(String partFileName, ResultSet resultSet) {
+	BufferedWriter resultsOutput = resultSet.getResultsOutput();
+	List<Result> results = resultSet.getResults();
+	for (int i = 0; i < results.size(); i++) {
+	    Result result = results.get(i);
+	    int id = result.getId();
+	    String title = result.getTitle();
+	    String address = result.getAddress();
+	    String city = result.getCity();
+	    String state = result.getState();
+	    String phone = result.getPhone();
+	    Double longitude = result.getLongitude();
+	    Double latitude = result.getLatitude();
 
-			Rating rating = result.getRating();
+	    Rating rating = result.getRating();
 
-			double distance = result.getDistance();
-			String url = result.getUrl();
-			String clickUrl = result.getClickUrl();
-			String mapUrl = result.getMapUrl();
-			String businessUrl = result.getBusinessUrl();
-			String businessClickUrl = result.getBusinessClickUrl();
+	    double distance = result.getDistance();
+	    String url = result.getUrl();
+	    String clickUrl = result.getClickUrl();
+	    String mapUrl = result.getMapUrl();
+	    String businessUrl = result.getBusinessUrl();
+	    String businessClickUrl = result.getBusinessClickUrl();
 
-			List<Category> categories = result.getCategories();
+	    List<Category> categories = result.getCategories();
 
-			try {
-				// file name:
-				resultsOutput.write(partFileName);
-				resultsOutput.write(";");
-				// other information:
-				resultsOutput.write(Integer.toString(id));
-				resultsOutput.write(";");
-				resultsOutput.write(title);
-				resultsOutput.write(";");
-				resultsOutput.write(city);
-				resultsOutput.write(";");
-				resultsOutput.write(state);
-				resultsOutput.write(";");
-				resultsOutput.write(Double.toString(latitude));
-				resultsOutput.write(";");
-				resultsOutput.write(Double.toString(longitude));
-				resultsOutput.write(";");
-				resultsOutput.write(Double.toString(distance));
-				for (int j = 0; j < categories.size(); j++) {
-					Category category = categories.get(j);
-					int categoryId = category.getId();
-					String categorynName = category.getName();
-					resultsOutput.write(";");
-					resultsOutput.write(Integer.toString(categoryId));
-					resultsOutput.write(";");
-					resultsOutput.write(categorynName);
-				}
-				resultsOutput.newLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+	    try {
+		// file name:
+		resultsOutput.write(partFileName);
+		resultsOutput.write(";");
+		// other information:
+		resultsOutput.write(Integer.toString(id));
+		resultsOutput.write(";");
+		resultsOutput.write(title);
+		resultsOutput.write(";");
+		resultsOutput.write(city);
+		resultsOutput.write(";");
+		resultsOutput.write(state);
+		resultsOutput.write(";");
+		resultsOutput.write(Double.toString(latitude));
+		resultsOutput.write(";");
+		resultsOutput.write(Double.toString(longitude));
+		resultsOutput.write(";");
+		resultsOutput.write(Double.toString(distance));
+		for (int j = 0; j < categories.size(); j++) {
+		    Category category = categories.get(j);
+		    int categoryId = category.getId();
+		    String categorynName = category.getName();
+		    resultsOutput.write(";");
+		    resultsOutput.write(Integer.toString(categoryId));
+		    resultsOutput.write(";");
+		    resultsOutput.write(categorynName);
 		}
+		resultsOutput.newLine();
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    }
 	}
+    }
 
 }
--- src/main/java/mo/umac/db/H2DB.java
+++ src/main/java/mo/umac/db/H2DB.java
@@ -1,5 +1,11 @@
 package mo.umac.db;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -21,6 +27,29 @@ import mo.umac.parser.ResultSet;
  */
 public class H2DB extends DataSet {
 
+    // sqls for creating table
+    private String sqlCreateItemTable = "CREATE TABLE ITEM (ITEMID INT PRIMARY KEY, TITLE VARCHAR(200), CITY VARCHAR(200), STATE VARCHAR(10), LATITUDE DOUBLE, LONGITUDE DOUBLE, DISTANCE DOUBLE, AVERAGERATING Svar, TOTALRATINGS DOUBLE, TOTALREVIEWS DOUBLE)";
+    private String sqlCreateCategoryTable = "CREATE TABLE CATEGORY (ITEMID INT, CATEGORYID INT, CATEGORYNAME VARCHAR(200))";
+    /**
+     * level: the divided level radius: the radius of the circle want to covered
+     */
+    private String sqlCreateQueryTable = "CREATE TABLE QUERY (QUERYID INT PRIMARY KEY, QUERY VARCHAR(100), ZIP INT, RESULTS INT, START INT, LATITUDE DOUBLE, LONGITUDE DOUBLE, RADIUS DOUBLE, LEVEL INT, PARENTID INT";
+
+    /**
+     * This table records that the item is returned by which query in which
+     * position.
+     */
+    private String sqlCreateQRR = "CREATE TABLE RELATIONSHIP (ITEMID INT, QEURYID INT, POSITION INT)";
+
+    // sqls preparation for insertion
+    private String sqlPrepItem = "INSERT INTO ITEM (ITEMID, TITLE, CITY, STATE, LATITUDE, LONGITUDE, DISTANCE, AVERAGERATING, TOTALRATINGS, TOTALREVIEWS) VALUES (?,?,?,?,?,?,?,?,?,?)";
+
+    private String sqlPrepCategory = "INSERT INTO CATEGORY (ITEMID, CATEGORYID, CATEGORYNAME) VALUES (?,?,?)";
+
+    private String sqlPrepQuery = "INSERT INTO QUERY (QUERYID, QUERY, ZIP, RESULTS, START, LATITUDE, LONGITUDE, RADIUS, LEVEL, PARENTID) VALUES (?,?,?,?,?,??,?,?,?)";
+
+    private String sqlPrepRelationship = "INSERT INTO RELATIONSHIP (ITEMID, QEURYID, POSITION), VALUES(?,?,?)";
+
     @Override
     public void record(int queryID, int level, int parentID,
 	    YahooLocalQuery qc, ResultSet resultSet) {
@@ -31,51 +60,29 @@ public class H2DB extends DataSet {
 	PreparedStatement prepQuery;
 	PreparedStatement prepRelationship;
 	try {
-	    prepItem = con
-		    .prepareStatement("INSERT INTO ITEM (ITEMID, TITLE, CITY, STATE, LATITUDE, LONGITUDE, DISTANCE, AVERAGERATING, TOTALRATINGS, TOTALREVIEWS) VALUES (?,?,?,?,?,?,?,?,?,?)");
-	    prepCategory = con
-		    .prepareStatement("INSERT INTO CATEGORY (ITEMID, CATEGORYID, CATEGORYNAME) VALUES (?,?,?)");
-	    prepQuery = con
-		    .prepareStatement("INSERT INTO QUERY (QUERYID, LATITUDE, LONGITUDE, RADIUS, LEVEL, PARENTID) VALUES (?,?,?,?,?,?)");
-	    prepRelationship = con
-		    .prepareStatement("INSERT INTO RELATIONSHIP (ITEMID, QEURYID, POSITION), VALUES(?,?,?)");
+	    prepItem = con.prepareStatement(sqlPrepItem);
+	    prepCategory = con.prepareStatement(sqlPrepCategory);
+	    prepQuery = con.prepareStatement(sqlPrepQuery);
+	    prepRelationship = con.prepareStatement(sqlPrepRelationship);
 	    List<Result> results = resultSet.getResults();
 	    for (int i = 0; i < results.size(); i++) {
 		Result result = results.get(i);
 		// table 1
-		prepItem.setInt(1, result.getId());
-		prepItem.setString(2, result.getTitle());
-		prepItem.setString(3, result.getCity());
-		prepItem.setString(4, result.getState());
-		prepItem.setDouble(5, result.getLatitude());
-		prepItem.setDouble(6, result.getLongitude());
-		prepItem.setDouble(7, result.getDistance());
-		Rating rating = result.getRating();
-		prepItem.setString(8, rating.getAverageRating());
-		prepItem.setDouble(9, rating.getTotalRatings());
-		prepItem.setDouble(10, rating.getTotalReviews());
+		setPrepItem(result, prepItem);
 		prepItem.addBatch();
 		// table 2
 		List<Category> listCategory = result.getCategories();
 		for (int j = 0; j < listCategory.size(); j++) {
 		    Category category = listCategory.get(j);
-		    prepCategory.setInt(1, result.getId());
-		    prepCategory.setInt(2, category.getId());
-		    prepCategory.setString(3, category.getName());
+		    setPrepCategory(result, category, prepCategory);
 		    prepCategory.addBatch();
 		}
 		// table 4
-		prepRelationship.setInt(1, result.getId());
-		prepRelationship.setInt(2, queryID);
-		prepRelationship.setInt(3, i+1);
+		setPrepRelationship(result, queryID, i, prepRelationship);
+		prepRelationship.addBatch();
 	    }
 	    // table 3
-	    prepQuery.setInt(1, queryID);
-	    prepQuery.setDouble(2, qc.getCircle().getCenter().y);
-	    prepQuery.setDouble(3, qc.getCircle().getCenter().x);
-	    prepQuery.setDouble(4, qc.getCircle().getRadius());
-	    prepQuery.setInt(5, level);
-	    prepQuery.setInt(6, parentID);
+	    setPrepQuery(queryID, qc, level, parentID, prepQuery);
 	    prepQuery.addBatch();
 
 	    con.setAutoCommit(false);
@@ -84,22 +91,90 @@ public class H2DB extends DataSet {
 	}
     }
 
-    private void createTables() {
-	String sqlQueryTable = sqlQueryTable();
-	String sqlItemTable = sqlItemTable();
-	String sqlCategoryTable = sqlCategoryTable();
-	String sqlQRR = sqlQueryResultsRelationship();
+    private PreparedStatement setPrepItem(Result result,
+	    PreparedStatement prepItem) {
+	try {
+	    prepItem.setInt(1, result.getId());
+	    prepItem.setString(2, result.getTitle());
+	    prepItem.setString(3, result.getCity());
+	    prepItem.setString(4, result.getState());
+	    prepItem.setDouble(5, result.getLatitude());
+	    prepItem.setDouble(6, result.getLongitude());
+	    prepItem.setDouble(7, result.getDistance());
+	    Rating rating = result.getRating();
+	    prepItem.setString(8, rating.getAverageRating());
+	    prepItem.setDouble(9, rating.getTotalRatings());
+	    prepItem.setDouble(10, rating.getTotalReviews());
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	}
+	return prepItem;
+    }
 
+    private PreparedStatement setPrepCategory(Result result, Category category,
+	    PreparedStatement prepCategory) {
+	try {
+	    prepCategory.setInt(1, result.getId());
+	    prepCategory.setInt(2, category.getId());
+	    prepCategory.setString(3, category.getName());
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	}
+	return prepCategory;
+    }
+
+    private PreparedStatement setPrepQuery(int queryID, YahooLocalQuery qc,
+	    int level, int parentID, PreparedStatement prepQuery) {
+	setPrepQuery(queryID, qc.getQuery(), qc.getZip(), qc.getResults(),
+		qc.getStart(), qc.getCircle().getCenter().y, qc.getCircle()
+			.getCenter().x, qc.getCircle().getRadius(), level,
+		parentID, prepQuery);
+	return prepQuery;
+    }
+
+    private PreparedStatement setPrepQuery(int queryID, String query, int zip,
+	    int results, int start, double latitude, double longitude,
+	    double radius, int level, int parentID, PreparedStatement prepQuery) {
+	try {
+	    prepQuery.setInt(1, queryID);
+	    prepQuery.setString(2, query);
+	    prepQuery.setInt(3, zip);
+	    prepQuery.setInt(4, results);
+	    prepQuery.setInt(5, start);
+	    prepQuery.setDouble(6, latitude);
+	    prepQuery.setDouble(7, longitude);
+	    prepQuery.setDouble(8, radius);
+	    prepQuery.setInt(9, level);
+	    prepQuery.setInt(10, parentID);
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	}
+	return prepQuery;
+    }
+
+    private PreparedStatement setPrepRelationship(Result result, int queryID,
+	    int position, PreparedStatement prepRelationship) {
+	try {
+	    prepRelationship.setInt(1, result.getId());
+	    prepRelationship.setInt(2, queryID);
+	    prepRelationship.setInt(3, position + 1);
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	}
+	return prepRelationship;
+    }
+
+    private void createTables() {
 	try {
 	    Class.forName("org.h2.Driver");
 	    Connection conn = DriverManager.getConnection(
 		    "jdbc:h2:file:../yahoolocal-h2/datasets;AUTO_SERVER=TRUE",
 		    "sa", "");
 	    Statement stat = conn.createStatement();
-	    stat.execute(sqlQueryTable);
-	    stat.execute(sqlItemTable);
-	    stat.execute(sqlCategoryTable);
-	    stat.execute(sqlQRR);
+	    stat.execute(sqlCreateQueryTable);
+	    stat.execute(sqlCreateItemTable);
+	    stat.execute(sqlCreateCategoryTable);
+	    stat.execute(sqlCreateQRR);
 	    stat.close();
 	    conn.close();
 	} catch (ClassNotFoundException e) {
@@ -109,52 +184,78 @@ public class H2DB extends DataSet {
 	}
     }
 
-    private String sqlItemTable() {
-	String sqlItemTable = "CREATE TABLE ITEM (ITEMID INT PRIMARY KEY, TITLE VARCHAR(200), CITY VARCHAR(200), STATE VARCHAR(10), LATITUDE DOUBLE, LONGITUDE DOUBLE, DISTANCE DOUBLE, AVERAGERATING Svar, TOTALRATINGS DOUBLE, TOTALREVIEWS DOUBLE)";
-	return sqlItemTable;
-    }
-
-    private String sqlCategoryTable() {
-	String sqlCategoryTable = "CREATE TABLE CATEGORY (ITEMID INT, CATEGORYID INT, CATEGORYNAME VARCHAR(200))";
-	return sqlCategoryTable;
-    }
-
-    /**
-     * level: the divided level radius: the radius of the circle want to covered
-     * 
-     * @return
-     */
-    private String sqlQueryTable() {
-	String sqlQueryTable = "CREATE TABLE QUERY (QUERYID INT PRIMARY KEY, LATITUDE DOUBLE, LONGITUDE DOUBLE, RADIUS DOUBLE, LEVEL INT, PARENTID INT";
-	return sqlQueryTable;
-    }
-
-    /**
-     * This table records that the item is returned by which query in which
-     * position.
-     * 
-     * @return
-     */
-    private String sqlQueryResultsRelationship() {
-	String sqlQRR = "CREATE TABLE RELATIONSHIP (ITEMID INT, QEURYID INT, POSITION INT)";
-	return sqlQRR;
-    }
-
     /**
      * Transfer the plain text dataset to the in-memory dataset: H2
      * 
-     * @param file
+     * @param folderPath
      * @param h2Name
      */
-    private void PlainTextToH2(String file, String h2Name) {
+    private void convertFileDBToH2DB(String folderPath, String h2Name) {
+	// looking for files: results and query
+	String queryFile = folderPath + "query";
+	String resultsFile = folderPath + "results";
 
+	// converting query file
+	BufferedReader br = null;
+	try {
+	    Class.forName("org.h2.Driver");
+	    Connection conn = DriverManager.getConnection(
+		    "jdbc:h2:file:../yahoolocal-h2/datasets;AUTO_SERVER=TRUE",
+		    "sa", "");
+	    br = new BufferedReader(new InputStreamReader(new FileInputStream(
+		    queryFile)));
+	    String data = null;
+	    String[] split;
+	    int queryID;
+	    int indexHyphenm;
+	    int indexDot;
+	    while ((data = br.readLine()) != null) {
+		data = data.trim();
+		split = data.split(";");
+		// query id
+		String queryIDString = split[0];
+		indexHyphenm = queryIDString.indexOf("-");
+		if (indexHyphenm != -1) {
+		    queryIDString = queryIDString.substring(0, indexHyphenm);
+		    queryID = Integer.parseInt(queryIDString);
+		} else {
+		    indexDot = queryIDString.indexOf(".xml");
+		    queryIDString = queryIDString.substring(0, indexDot);
+		    queryID = Integer.parseInt(queryIDString);
+		}
+		// query Info
+		String query = split[1];
+		int zip = Integer.parseInt(split[2]);
+		int results = Integer.parseInt(split[3]);
+		int start = Integer.parseInt(split[4]);
+		double latitude = Double.parseDouble(split[5]);
+		double longitude = Double.parseDouble(split[6]);
+		double radius = Double.parseDouble(split[7]);
+		int level = -1;
+		int parentID = -1;
+
+		// begin to prepare the statements
+		try {
+		    PreparedStatement prepQuery = conn
+			    .prepareStatement(sqlPrepQuery);
+		    setPrepQuery(queryID, query, zip, results, start, latitude,
+			    longitude, radius, level, parentID, prepQuery);
+		    prepQuery.addBatch();
+
+		} catch (SQLException e) {
+		    e.printStackTrace();
+		}
+
+	    }
+	    br.close();
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	} catch (ClassNotFoundException e1) {
+	    e1.printStackTrace();
+	} catch (SQLException e1) {
+	    e1.printStackTrace();
+	}
     }
-
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-
-    }
-
 }
